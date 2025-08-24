702349165d03ae244e928bc7b8903cf7d14ea48b
Tikhon-code <markjonsonsigma@gmail.com>
2025-08-24 14:49:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 #!/bin/python
 import requests
+import zipfile
 import sys
+import os
+
 
 def load(to_file, file):
-    print(f"{to_file}{file}")
-    URL = requests.get(f"http://192.168.1.100:8000/repository/{to_file}{file}")
-    with open(f"files/{file}", 'wb') as f:
-        f.write(URL.content)
+    print(f"Downloading: {file}")
+    URL = requests.get(f"https://raw.githubusercontent.com/Tikhon-code/package-manager/refs/heads/main/repository/{to_file}{file}", stream=True)
+    if URL.status_code == 200:
+        with open(f"files/{file}", 'wb') as f:
+            f.write(URL.content)
+
+    else:
+        print(f"File: {file} not found in {to_file}")
+        return 1
 
     
 
@@ -14,15 +22,16 @@ def load_packages(for_file, file):
     with open(f"{for_file}{file}", 'r') as f:
         for line in f:
             load('packages/', line.strip())
+            if line.strip().endswith('zip'):
+                with zipfile.ZipFile(f"{for_file}{line}".strip(), 'r') as zip:
+                    zip.extractall(for_file)
+                os.remove(f"{for_file}{line}".strip())
 
     
-
 def main(file):
-    try:
-        load('info/', f"{file}.info")
+    if load('info/', f"{file}.info") != 1:
         load_packages('files/', f"{file}.info")
-    except Exception as e:
-        print(e)
+        os.remove(f'files/{file}.info')
 
 if len(sys.argv[1:]) >= 1:
     main(sys.argv[1])
