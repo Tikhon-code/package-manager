342aec08db7dc55618402f8aeaf5b12b0a796ef3
Tikhon-code <markjonsonsigma@gmail.com>
2025-08-24 19:14:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ import os
 
 def start(startind=0):
     global now
-    if startind == 0:
-        now = datetime.datetime.now()
-    else:
-        now = startind
+    now = datetime.datetime.now()
+
 
 def end():
     end = datetime.datetime.now()
@@ -26,22 +24,14 @@ def show_packages(for_file, file):
         for line in f:
             line = line.strip()
             print(line)
-    
-    '''
-    yesorno = input("Download? Y/n: ")
-    if yesorno.lower() == "y" or yesorno.strip() == "":
-       pass
-    else:
-        os.remove(f"{for_file}{file}")
-        sys.exit(0)
-    '''
 
 def load(to_file, file, sub=False):
     global process
     if sub == False:
-        os.system(f'luajit request.lua {to_file} {file}')
+        os.system(f'lua request.lua {to_file} {file}')
     else:
-        process = subprocess.Popen(['luajit', 'request.lua', to_file, file])
+        process = subprocess.Popen(['lua', 'request.lua', to_file, file])
+    
 
 
 async def load_packages(for_file, file):
@@ -68,7 +58,6 @@ def main(file):
     asyncio.run(load_packages('files/', f"{file}.info"))
     os.remove(f'files/{file}.info')
     print(f"Downloading complete, finish time: {end()}")
-
 
 if len(sys.argv[1:]) >= 1:
     main(sys.argv[1])
